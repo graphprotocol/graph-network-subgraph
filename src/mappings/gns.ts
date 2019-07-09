bc30744466062d49323b94936cd6242902a2eeed
David Kajpust <kajpustd@gmail.com>
2019-07-09 11:30:38 -0400

    Updates to the gns mapping

--- src/mappings/gns.ts
+++ src/mappings/gns.ts
@@ -1,4 +1,4 @@
-import {BigInt} from '@graphprotocol/graph-ts'
+import { log } from '@graphprotocol/graph-ts'
 import {
   DomainAdded,
   DomainTransferred,
@@ -26,21 +26,30 @@ export function handleDomainTransferred(event: DomainTransferred): void {
 }
 
 export function handleSubgraphIdAdded(event: SubgraphIdAdded): void {
-  // Subdomain string is blank
-  if (event.params.subdomainHash.toHexString() == "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470") {
+  log.debug(
+    'Subdomain Hash: {}',
+    [
+      event.params.subdomainHash.toHexString()
+    ]
+  )
+
+  // Subdomain string is not blank, therefore we are indexing a tld
+  if (event.params.subdomainHash.toHexString() != "0x1c47c222430ce3cff6bbf3ce14d1374f52c32d129ef0ce041af2c4eea5ff81e2") {
     let id = event.params.topLevelDomainHash.toHex()
+    let domain = new Domain(id)
+    domain.subgraphID = event.params.subgraphId
+    domain.metadataHash = event.params.ipfsHash
+
+    domain.save()
+
+    // We are indexing a subdomain, since the name is not blank
+  } else {
+    let id = event.params.subdomainHash.toHex()
     let domain = new Domain(id)
     domain.subgraphID = event.params.subgraphId
     domain.metadataHash = event.params.ipfsHash
     domain.parentDomain = event.params.topLevelDomainHash
     domain.name = event.params.subdomainName
-    domain.save()
-
-  } else {
-    let id = event.params.topLevelDomainHash.toHex()
-    let domain = new Domain(id)
-    domain.subgraphID = event.params.subgraphId
-    domain.metadataHash = event.params.ipfsHash
     domain.save()
     // name is added previously when domain is registered
   }
