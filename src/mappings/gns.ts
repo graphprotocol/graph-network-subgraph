68b3778c5d3fa49702d1c2c7b22a698475a8c1ae
Nena <nevena.djaja@gmail.com>
2019-08-08 11:59:57 -0700

    Edit schema and mappings to use Subgraph entity

--- src/mappings/gns.ts
+++ src/mappings/gns.ts
@@ -1,4 +1,4 @@
-import {store} from '@graphprotocol/graph-ts'
+import { store, ByteArray, Bytes, ipfs, json, log } from '@graphprotocol/graph-ts'
 import {
   DomainAdded,
   DomainTransferred,
@@ -8,21 +8,23 @@ import {
   AccountMetadataChanged,
   SubgraphMetadataChanged,
 } from '../../generated/GNS/GNS'
-import {Domain, Account, Subgraph} from '../../generated/schema'
+import { Domain, Account, Subgraph } from '../../generated/schema'
 
+// new domain is treated as a Subgraph entity with parent set to null
 export function handleDomainAdded(event: DomainAdded): void {
   let id = event.params.topLevelDomainHash.toHexString()
-  let domain = new Domain(id)
-  domain.name = event.params.domainName
-  domain.owner = event.params.owner
-  domain.save()
+  let subgraph = new Subgraph(id)
+  subgraph.name = event.params.domainName
+  subgraph.owner = event.params.owner
+  subgraph.parent = null
+  subgraph.save()
 }
 
 export function handleDomainTransferred(event: DomainTransferred): void {
   let id = event.params.domainHash.toHexString()
-  let domain = new Domain(id)
-  domain.owner = event.params.newOwner
-  domain.save()
+  let subgraph = new Subgraph(id)
+  subgraph.owner = event.params.newOwner
+  subgraph.save()
 }
 
 export function handleSubgraphCreated(event: SubgraphCreated): void {
@@ -33,8 +35,10 @@ export function handleSubgraphCreated(event: SubgraphCreated): void {
 
   // The subddomain is blank, i.e. we are registered to the TLD
   // We ignore the subdomain name (it is blank), and the domain hash is the same as the TLD
-  if (event.params.registeredHash.toHexString() ==
-    '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470') {
+  if (
+    event.params.registeredHash.toHexString() ==
+    '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470'
+  ) {
     let id = event.params.topLevelDomainHash.toHexString()
     let domain = new Domain(id)
     domain.save()
@@ -48,6 +52,7 @@ export function handleSubgraphCreated(event: SubgraphCreated): void {
   }
 }
 
+// NOTE: this is not inside of subgraph.yaml
 export function handleUpdateDomainSubgraphID(event: SubgraphIDUpdated): void {
   let id = event.params.domainHash.toHexString()
   let domain = new Domain(id)
@@ -62,7 +67,7 @@ export function handleUpdateDomainSubgraphID(event: SubgraphIDUpdated): void {
 
 export function handleDomainDeleted(event: DomainDeleted): void {
   let id = event.params.domainHash.toHexString()
-  store.remove("Domain", id)
+  store.remove('Subgraph', id)
 }
 
 export function handleAccountMetadataChanged(event: AccountMetadataChanged): void {
@@ -72,9 +77,46 @@ export function handleAccountMetadataChanged(event: AccountMetadataChanged): voi
   account.save()
 }
 
+export function handleSubgraphIDUpdated(event: SubgraphIDUpdated): void {
+  let id = event.params.domainHash.toHexString()
+  let subgraph = new Subgraph(id)
+}
+
+/* Params:
+  - domainHash - becomes a created Subgraph ID
+  - ipfsHash - IPFS hash for subgraph metadata
+*/
 export function handleSubgraphMetadataChanged(event: SubgraphMetadataChanged): void {
   let id = event.params.domainHash.toHexString()
-  let domain = new Domain(id)
-  domain.metadataHash = event.params.ipfsHash
-  domain.save()
+  let subgraph = new Subgraph(id)
+  subgraph.metadataHash = event.params.ipfsHash
+  subgraph.save()
+
+  let hexHash = addQm(event.params.ipfsHash) as Bytes
+  let base58Hash = hexHash.toBase58() // imported crypto function
+
+  // read subgraph metadata from IPFS
+  let getSubgraphDataFromIPFS = ipfs.cat(base58Hash)
+  if (getSubgraphDataFromIPFS !== null) {
+    let data = json.fromBytes(getSubgraphDataFromIPFS as Bytes).toObject()
+    let subgraph = new Subgraph(id)
+    subgraph.name = data.get('name').toString()
+    subgraph.displayName = data.get('displayName').toString()
+    subgraph.subtitle = data.get('subtitle').toString()
+    subgraph.image = data.get('image').toString()
+    subgraph.description = data.get('description').toString()
+    subgraph.githubUrl = data.get('githubUrl').toString()
+    subgraph.save()
+  }
+}
+
+/*     HELPERS     */
+export function addQm(a: ByteArray): ByteArray {
+  let out = new Uint8Array(34)
+  out[0] = 0x12
+  out[1] = 0x20
+  for (let i = 0; i < 32; i++) {
+    out[i + 2] = a[i]
+  }
+  return out as ByteArray
 }
