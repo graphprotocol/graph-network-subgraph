bcde9177cdccbd6720f04843669ee60c1dc0b19a
Nena <nevena.djaja@gmail.com>
2019-08-09 10:02:03 -0700

    Rename IndexNodeInfo to IndexerInfo

--- src/mappings/staking.ts
+++ src/mappings/staking.ts
@@ -6,15 +6,18 @@ import {
   IndexingNodeFinalizeLogout,
   DisputeCreated,
   DisputeAccepted,
-  DisputeRejected
+  DisputeRejected,
 } from '../../generated/Staking/Staking'
-import {CuratorInfo, IndexNodeInfo, Subgraph, Transactions} from '../../generated/schema'
-import {BigInt, store} from '@graphprotocol/graph-ts'
+import { CuratorInfo, IndexerInfo, Subgraph, Transactions } from '../../generated/schema'
+import { BigInt, store } from '@graphprotocol/graph-ts'
 
 // TODO - note, this mapping is far from complete, needs to be updated with the new schema
 
 export function handleCuratorStaked(event: CuratorStaked): void {
-  let id = event.params.staker.toHexString().concat("-").concat(event.params.subgraphID.toHexString())
+  let id = event.params.staker
+    .toHexString()
+    .concat('-')
+    .concat(event.params.subgraphID.toHexString())
   let curator = new CuratorInfo(id)
   curator.subgraphID = event.params.subgraphID
   curator.tokensStaked = event.params.amountStaked
@@ -29,7 +32,10 @@ export function handleCuratorStaked(event: CuratorStaked): void {
 }
 
 export function handleCuratorLogout(event: CuratorLogout): void {
-  let id = event.params.staker.toHexString().concat("-").concat(event.params.subgraphID.toHexString())
+  let id = event.params.staker
+    .toHexString()
+    .concat('-')
+    .concat(event.params.subgraphID.toHexString())
   store.remove('Curator', id)
 
   let subgraph = new Subgraph(event.params.subgraphID.toHexString())
@@ -39,13 +45,16 @@ export function handleCuratorLogout(event: CuratorLogout): void {
 }
 
 export function handleIndexingNodeStaked(event: IndexingNodeStaked): void {
-  let id = event.params.staker.toHexString().concat("-").concat(event.params.subgraphID.toHexString())
+  let id = event.params.staker
+    .toHexString()
+    .concat('-')
+    .concat(event.params.subgraphID.toHexString())
 
   // Subgraph SHOULD already exist, the GNS must create it before anyone can stake on it
   let subgraph = Subgraph.load(event.params.subgraphID.toHexString())
 
   // Need to load to check if this is a new index node, so we can add 1 to total indexers
-  let indexNode = IndexNodeInfo.load(id)
+  let indexNode = IndexerInfo.load(id)
   if (indexNode == null) {
     indexNode.user = event.params.staker.toHexString()
     indexNode.subgraphID = event.params.subgraphID
@@ -56,20 +65,27 @@ export function handleIndexingNodeStaked(event: IndexingNodeStaked): void {
 
   subgraph.totalIndexingStake = event.params.subgraphTotalIndexingStake
   subgraph.save()
-
 }
 
 export function handleIndexingNodeBeginLogout(event: IndexingNodeBeginLogout): void {
-  let id = event.params.staker.toHexString().concat("-").concat(event.params.subgraphID.toHexString())
-  let indexNode = new IndexNodeInfo(id)
+  let id = event.params.staker
+    .toHexString()
+    .concat('-')
+    .concat(event.params.subgraphID.toHexString())
+  let indexNode = new IndexerInfo(id)
   indexNode.logoutStartTime = event.block.timestamp
   indexNode.save()
 }
 
-export function handleIndexingNodeFinalizeLogout(event: IndexingNodeFinalizeLogout): void {
-  let id = event.params.staker.toHexString().concat("-").concat(event.params.subgraphID.toHexString())
+export function handleIndexingNodeFinalizeLogout(
+  event: IndexingNodeFinalizeLogout,
+): void {
+  let id = event.params.staker
+    .toHexString()
+    .concat('-')
+    .concat(event.params.subgraphID.toHexString())
 
-  let indexNode = new IndexNodeInfo(id)
+  let indexNode = new IndexerInfo(id)
   indexNode.logoutStartTime = 0
   indexNode.tokensStaked = BigInt.fromI32(0)
   indexNode.save()
@@ -79,12 +95,8 @@ export function handleIndexingNodeFinalizeLogout(event: IndexingNodeFinalizeLogo
   subgraph.save()
 }
 
+export function handleDisputeCreated(event: DisputeCreated): void {}
 
-export function handleDisputeCreated(event: DisputeCreated): void {
-}
+export function handleDisputeAccepted(event: DisputeAccepted): void {}
 
-export function handleDisputeAccepted(event: DisputeAccepted): void {
-}
-
-export function handleDisputeRejected(event: DisputeRejected): void {
-}
+export function handleDisputeRejected(event: DisputeRejected): void {}
