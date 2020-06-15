f814129ff2e807d93e21f081545c1dd7678cf555
David Kajpust <kajpustd@gmail.com>
2020-06-15 18:14:07 -0400

    Get GNS resolving names working. Split into 5 functions

--- src/mappings/gns.ts
+++ src/mappings/gns.ts
@@ -1,7 +1,6 @@
 import { Bytes, ipfs, json, BigInt, log } from '@graphprotocol/graph-ts'
 import { SubgraphPublished, SubgraphDeprecated, GNS } from '../types/GNS/GNS'
 
-
 import { Subgraph, SubgraphDeployment, SubgraphVersion, GraphAccount } from '../types/schema'
 
 import { jsonToString } from './utils'
@@ -11,8 +10,7 @@ import {
   createGraphAccount,
   getVersionNumber,
   addQm,
-  simpleNamehash,
-  verifyName
+  resolveName,
 } from './helpers'
 
 /**
@@ -30,28 +28,6 @@ export function handleSubgraphPublished(event: SubgraphPublished): void {
 
   let versionNumber = getVersionNumber(graphAccount, subgraphNumber, BigInt.fromI32(1))
   let versionID = subgraphID.concat('-').concat(versionNumber.toString())
-
-  // Resolve name
-  let name = simpleNamehash(event.params.name, event.params.nameIdentifier.toHexString())
-
-  // Verify name
-  name = verifyName(graphAccount, name, event.params.nameIdentifier)
-
-  // When name is not null, it is proven it is owned
-  // Next, we must make sure that the name is not already in use
-  // If it is, name is set to null
-  if (name != null) {
-    let gns = GNS.bind(event.address)
-    let subgraphCount = gns.graphAccountSubgraphNumbers(event.params.graphAccount).toI32()
-    for (let i = 0; i < subgraphCount; i++) {
-      let counter = BigInt.fromI32(i).toString()
-      let checkSubgraph = Subgraph.load(graphAccount.concat('-').concat(counter))
-      if (checkSubgraph.name == name) {
-        name = null
-        break
-      }
-    }
-  }
 
   // Update subgraph
   let subgraph = Subgraph.load(subgraphID)
@@ -72,8 +48,41 @@ export function handleSubgraphPublished(event: SubgraphPublished): void {
     subgraph.currentVersion = versionID
   }
 
+  // Resolve name
+  let graphAccountNameID = resolveName(
+    event.params.graphAccount,
+    event.params.name,
+    event.params.nameIdentifier,
+  )
+  // let name = checkTLD(event.params.name, event.params.nameIdentifier.toHexString())
+
+  // // Verify name
+  // let graphAccountNameID: string
+  // if (name != null) {
+  //   graphAccountNameID = verifyNameOwnership(graphAccount, name, event.params.nameIdentifier)
+  // }
+  // // When name is not null, it is proven it is owned
+  // // Next, we must make sure that the name is not already in use
+  // // We can't check the ID, we need to check the actually STRING, to prevent the same
+  // // string being used for one account, but from two different name systems
+  // // If it is, name is set to null
+  // if (graphAccountNameID != null) {
+  //   const words = graphAccountNameID.split('-')
+  //   name = words[1]
+  //   let gns = GNS.bind(event.address)
+  //   let subgraphCount = gns.graphAccountSubgraphNumbers(event.params.graphAccount).toI32()
+  //   for (let i = 0; i < subgraphCount; i++) {
+  //     let counter = BigInt.fromI32(i).toString()
+  //     let checkSubgraph = Subgraph.load(graphAccount.concat('-').concat(counter))
+  //     if (checkSubgraph.name == name) {
+  //       name = null
+  //       break
+  //     }
+  //   }
+  // }
+
   // Set name
-  subgraph.name = name
+  subgraph.name = graphAccountNameID
 
   // IPFS hash contains SubgraphVersion metadata, as well as Subgraph metadata
   // Subgraph metadata is always updated completely, with the JSON upload having
