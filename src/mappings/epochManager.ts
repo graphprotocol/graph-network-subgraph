a3c902199c227f64d5e39e7e90a7f3eac7e7118a
David Kajpust <kajpustd@gmail.com>
2020-08-18 10:37:41 -0400

    Pushing backfill code to github before removing for good

--- src/mappings/epochManager.ts
+++ src/mappings/epochManager.ts
@@ -1,6 +1,6 @@
 import { GraphNetwork } from '../types/schema'
 import { EpochRun, EpochLengthUpdate } from '../types/EpochManager/EpochManager'
-import { createOrLoadGraphNetwork, createOrLoadEpoch } from './helpers'
+import { createOrLoadGraphNetwork, createOrLoadEpoch, createEpoch } from './helpers'
 
 /**
  * @dev handleEpochRun
@@ -18,12 +18,30 @@ export function handleEpochRun(event: EpochRun): void {
  */
 export function handleEpochLengthUpdate(event: EpochLengthUpdate): void {
   let graphNetwork = createOrLoadGraphNetwork()
+  let startingEpochLength = graphNetwork.epochLength
   graphNetwork.epochLength = event.params.epochLength.toI32()
   graphNetwork.lastLengthUpdateEpoch = graphNetwork.currentEpoch
   graphNetwork.lastLengthUpdateBlock = event.block.number.toI32()
-  graphNetwork.save()
 
+  // Edge case where at the very start, we save it ahead, otherwise divide by zero error.
+  // related to the comment below
+  if (startingEpochLength == 0) {
+    graphNetwork.save()
+  }
+
+  // This returns a new epoch, or current epoch, with the old epoch length
   let epoch = createOrLoadEpoch(event.block.number)
-  epoch.endBlock = epoch.startBlock + graphNetwork.epochLength
+
+  // We must take this epoch, update its blocks and end it, and make a new epoch
+  // Since whenever we update epoch length we end the current epoch and create a new one
+  epoch.endBlock = event.block.number.toI32()
   epoch.save()
+  createEpoch(
+    event.block.number.toI32(),
+    graphNetwork.epochLength,
+    graphNetwork.currentEpoch + 1,
+  )
+
+  // We now save the new length. If done earlier, if would back fill with the wrong epoch length
+  graphNetwork.save()
 }
