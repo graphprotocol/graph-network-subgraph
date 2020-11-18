bdc3fb8d9a81cfe542066d48c135aaea1064c993
David Kajpust <kajpustd@gmail.com>
2020-11-17 19:42:32 -0500

    Fix epochs to start at 0, and simplify logic

--- src/mappings/epochManager.ts
+++ src/mappings/epochManager.ts
@@ -1,8 +1,5 @@
 import { GraphNetwork } from '../types/schema'
-import {
-  EpochRun,
-  EpochLengthUpdate,
-} from '../types/EpochManager/EpochManager'
+import { EpochRun, EpochLengthUpdate } from '../types/EpochManager/EpochManager'
 import { createOrLoadEpoch, createEpoch } from './helpers'
 import { log, BigInt } from '@graphprotocol/graph-ts'
 
@@ -23,22 +20,27 @@ export function handleEpochRun(event: EpochRun): void {
 export function handleEpochLengthUpdate(event: EpochLengthUpdate): void {
   let graphNetwork = GraphNetwork.load('1')
 
-  // Edge case to handle when it is the first time, otherwise createOrLoadEpoch fails
-  // TODO = make this an event emitted in EpochManager constructor
-  // And have this event CLEARLY create the first epoch and update graphNetwork
-  if (graphNetwork.epochLength == 0) {
+  // This event is emitted on EpochManagers constructor, so it has some special logic to handle
+  // initialization here
+  if (graphNetwork.epochLength == 0) { // Will only ever be 0 on initialization in the contracts
     graphNetwork.epochLength = event.params.epochLength.toI32()
+    graphNetwork.lastLengthUpdateBlock = event.block.number.toI32()
+    graphNetwork.currentEpoch = 0
+    graphNetwork.epochCount = 1
+    graphNetwork.lastLengthUpdateEpoch = graphNetwork.currentEpoch
     graphNetwork.save()
+
+    createEpoch(event.block.number.toI32(), graphNetwork.epochLength, graphNetwork.currentEpoch)
+    // return here so it doesn't run the normal handler
+    return
   }
 
   // This returns a new epoch, or current epoch, with the old epoch length
   let epoch = createOrLoadEpoch(event.block.number)
   // We must take this epoch, update its blocks and end it, and make a new epoch
-  // Since whenever we update epoch length we end the current epoch and create a new one
-  // UNLESS it is the first epoch
-  if (epoch.id != '1') {
-    epoch.endBlock = event.block.number.toI32()
-  }
+  // Since whenever we update epoch length in the contracts we end the current epoch
+  // and create a new one
+  epoch.endBlock = event.block.number.toI32()
   epoch.save()
 
   // Now it is safe to update graphNetwork, since the past epoch is completed
@@ -48,14 +50,11 @@ export function handleEpochLengthUpdate(event: EpochLengthUpdate): void {
   graphNetwork.lastLengthUpdateBlock = event.block.number.toI32()
 
   // Now we create the new epoch that starts fresh with the new length,
-  // Update graphNetwork for the new epoch
   let newEpochNumber = graphNetwork.currentEpoch + 1
+  createEpoch(event.block.number.toI32(), graphNetwork.epochLength, newEpochNumber)
 
-  // create the new epoch, if not first epoch
-  if (epoch.id != '1') {
-    graphNetwork.currentEpoch = newEpochNumber
-    createEpoch(event.block.number.toI32(), graphNetwork.epochLength, newEpochNumber)
-  }
+  // Update graphNetwork for the new epoch
+  graphNetwork.currentEpoch = newEpochNumber
   graphNetwork.lastLengthUpdateEpoch = graphNetwork.currentEpoch
   graphNetwork.save()
 }
