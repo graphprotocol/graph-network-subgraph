98bac8f1171352e8fc6326b700de444060f772b7
David Kajpust <kajpustd@gmail.com>
2020-12-16 15:54:25 -0600

    Fix script for mainnet

---
+++ config/mainnetAddressScript.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs'
+import * as mustache from 'mustache'
+import * as networkAddresses from '@graphprotocol/contracts/addresses.json'
+import { Addresses } from './addresses.template'
+
+// mustache doesn't like numbered object keys
+let renameAddresses: any = networkAddresses
+renameAddresses['mainnet'] = networkAddresses['1']
+
+export let addresses: Addresses = {
+  controller: '{{mainnet.Controller.address}}',
+  graphToken: '{{mainnet.GraphToken.address}}',
+  epochManager: '{{mainnet.EpochManager.address}}',
+  disputeManager: '{{mainnet.DisputeManager.address}}',
+  staking: '{{mainnet.Staking.address}}',
+  curation: '{{mainnet.Curation.address}}',
+  rewardsManager: '{{mainnet.RewardsManager.address}}',
+  serviceRegistry: '{{mainnet.ServiceRegistry.address}}',
+  gns: '{{mainnet.GNS.address}}',
+  ens: '{{mainnet.IENS.address}}',
+  ensPublicResolver: '{{mainnet.IPublicResolver}}',
+  blockNumber: '',
+  network: '',
+}
+
+const main = (): void => {
+  try {
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
+    output.blockNumber = '11440000' // Hardcoded a few thousand blocks before 1st contract deployed
+    output.network = 'mainnet' // Todo - make dynamic
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+  } catch (e) {
+    console.log(`Error saving artifacts: ${e.message}`)
+  }
+}
+
+main()
