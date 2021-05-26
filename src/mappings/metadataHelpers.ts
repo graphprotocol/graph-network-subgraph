2c8bf90cc21cea7ac699ba4bacb095b9cedb7a24
Jannis Pohlmann <jannis@edgeandnode.com>
2021-05-26 16:28:44 +0200

    Address review comments

--- src/mappings/metadataHelpers.ts
+++ src/mappings/metadataHelpers.ts
@@ -3,10 +3,56 @@ import { GraphAccount, Subgraph, SubgraphVersion } from '../types/schema'
 import { jsonToString } from './utils'
 
 export function fetchGraphAccountMetadata(graphAccount: GraphAccount, ipfsHash: string): void {
+  let ipfsData = ipfs.cat(ipfsHash)
+  if (ipfsData != null) {
+    let data = json.fromBytes(ipfsData as Bytes).toObject()
+    graphAccount.codeRepository = jsonToString(data.get('codeRepository'))
+    graphAccount.description = jsonToString(data.get('description'))
+    graphAccount.image = jsonToString(data.get('image'))
+    graphAccount.displayName = jsonToString(data.get('displayName'))
+    let isOrganization = data.get('isOrganization')
+    if (isOrganization != null && isOrganization.kind === JSONValueKind.BOOL) {
+      graphAccount.isOrganization = isOrganization.toBool()
+    }
+    graphAccount.website = jsonToString(data.get('website'))
+    graphAccount.save()
+  }
 }
 
-export function fetchSubgraphMetadata(subgraph: Subgraph, ipfsHash: string): void {
+export function fetchSubgraphMetadata(subgraph: Subgraph, ipfsHash: string): Subgraph {
+  let metadata = ipfs.cat(ipfsHash)
+  if (metadata !== null) {
+    let tryData = json.try_fromBytes(metadata as Bytes)
+    if (tryData.isOk) {
+      let data = tryData.value.toObject()
+      subgraph.description = jsonToString(data.get('description'))
+      subgraph.image = jsonToString(data.get('image'))
+      subgraph.displayName = jsonToString(data.get('displayName'))
+      subgraph.codeRepository = jsonToString(data.get('codeRepository'))
+      subgraph.website = jsonToString(data.get('website'))
+    } else {
+      subgraph.description = ''
+      subgraph.image = ''
+      subgraph.displayName = ''
+      subgraph.codeRepository = ''
+      subgraph.website = ''
+    }
+  }
+  return subgraph
 }
 
-export function fetchSubgraphVersionMetadata(subgraph: SubgraphVersion, ipfsHash: string): void {
+export function fetchSubgraphVersionMetadata(subgraphVersion: SubgraphVersion, ipfsHash: string): SubgraphVersion {
+  let getVersionDataFromIPFS = ipfs.cat(ipfsHash)
+  if (getVersionDataFromIPFS !== null) {
+    let tryData = json.try_fromBytes(getVersionDataFromIPFS as Bytes)
+    if (tryData.isOk) {
+      let data = tryData.value.toObject()
+      subgraphVersion.description = jsonToString(data.get('description'))
+      subgraphVersion.label = jsonToString(data.get('label'))
+    } else {
+      subgraphVersion.description = ''
+      subgraphVersion.label = ''
+    }
+  }
+  return subgraphVersion
 }
