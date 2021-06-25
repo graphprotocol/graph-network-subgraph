9dac5f9704e206f16a18d02b90afdb729c576dac
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2021-06-25 13:15:35 -0300

    Added manual parsing of the manifest file for network and schema

--- src/mappings/metadataHelpers.template.ts
+++ src/mappings/metadataHelpers.template.ts
@@ -1,5 +1,5 @@
 import { json, ipfs, Bytes, JSONValueKind, log } from '@graphprotocol/graph-ts'
-import { GraphAccount, Subgraph, SubgraphVersion } from '../types/schema'
+import { GraphAccount, Subgraph, SubgraphVersion, SubgraphDeployment } from '../types/schema'
 import { jsonToString } from './utils'
 import { createOrLoadSubgraphCategory, createOrLoadSubgraphCategoryRelation, createOrLoadNetwork } from './helpers'
 
@@ -73,4 +73,31 @@ export function fetchSubgraphVersionMetadata(subgraphVersion: SubgraphVersion, i
   }
   {{/ipfs}}
   return subgraphVersion
+}
+
+export function fetchSubgraphDeploymentManifest(deployment: SubgraphDeployment, ipfsHash: string): SubgraphDeployment {
+  {{#ipfs}}
+  let getManifestFromIPFS = ipfs.cat(ipfsHash)
+  if (getManifestFromIPFS !== null) {
+    deployment.manifest = getManifestFromIPFS.toString()
+
+    let manifest = deployment.manifest
+    // we take the right side of the split, since it's the one which will have the schema ipfs hash
+    let schemaSplit = manifest.split('schema:\n', 2)[1]
+    let schemaFileSplit = schemaSplit.split('/ipfs/', 2)[1]
+    let schemaIpfsHash = schemaFileSplit.split('\n', 2)[0]
+    deployment.schemaIpfsHash = schemaIpfsHash
+
+    let getSchemaFromIPFS = ipfs.cat(schemaIpfsHash)
+    if (getSchemaFromIPFS !== null) {
+      deployment.schema = getSchemaFromIPFS.toString()
+    }
+
+    // We get the first occurrence of `network` since subgraphs can only have data sources for the same network
+    let networkSplit = manifest.split('network: ', 2)[1]
+    let network = networkSplit.split('\n', 2)[0]
+    deployment.network = network
+  }
+  {{/ipfs}}
+  return deployment as SubgraphDeployment
 }
