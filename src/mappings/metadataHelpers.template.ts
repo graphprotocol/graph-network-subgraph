d09d84dadfa29b7ff2bde1719cd0e97cfe1a8048
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2022-04-27 17:07:24 -0300

    Removed debug logs

--- src/mappings/metadataHelpers.template.ts
+++ src/mappings/metadataHelpers.template.ts
@@ -104,49 +104,41 @@ export function fetchSubgraphDeploymentManifest(deployment: SubgraphDeployment, 
     deployment.manifest = getManifestFromIPFS.toString()
 
     let manifest = deployment.manifest!
-    log.warning("BEFORE PARSING LOGIC FOR {}", [ipfsHash])
     // we take the right side of the split, since it's the one which will have the schema ipfs hash
     let schemaSplitTry = manifest.split('schema:\n', 2)
     if (schemaSplitTry.length == 2) {
       let schemaSplit = schemaSplitTry[1]
-      log.warning("After schema:\n split for {}", [ipfsHash])
 
       let schemaFileSplitTry = schemaSplit.split('/ipfs/', 2)
       if (schemaFileSplitTry.length == 2) {
         let schemaFileSplit = schemaFileSplitTry[1]
-        log.warning("After /ipfs/ split for {}", [ipfsHash])
 
         let schemaIpfsHashTry = schemaFileSplit.split('\n', 2)
         if (schemaIpfsHashTry.length == 2) {
           let schemaIpfsHash = schemaIpfsHashTry[0]
-          log.warning("After \n (schema) split for {}", [ipfsHash])
           deployment.schemaIpfsHash = schemaIpfsHash
-          log.warning("After getting schemaIpfsHash for {}", [ipfsHash])
 
           let getSchemaFromIPFS = ipfs.cat(schemaIpfsHash)
           if (getSchemaFromIPFS !== null) {
             deployment.schema = getSchemaFromIPFS.toString()
-            log.warning("Successful ipfs.cat for schema for {}", [ipfsHash])
           }
         } else {
-          log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema can't be parsed. Error: schemaIpfsHashTry.length isn't 2, actual length: {}", [ipfsHash, schemaIpfsHashTry.length.toString()])
+          log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema file hash can't be retrieved. Error: schemaIpfsHashTry.length isn't 2, actual length: {}", [ipfsHash, schemaIpfsHashTry.length.toString()])
         }
       } else {
-        log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema can't be parsed. Error: schemaFileSplitTry.length isn't 2, actual length: {}", [ipfsHash, schemaFileSplitTry.length.toString()])
+        log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema file hash can't be retrieved. Error: schemaFileSplitTry.length isn't 2, actual length: {}", [ipfsHash, schemaFileSplitTry.length.toString()])
       }
     } else {
-      log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema can't be parsed. Error: schemaSplitTry.length isn't 2, actual length: {}", [ipfsHash, schemaSplitTry.length.toString()])
+      log.warning("[MANIFEST PARSING FAIL] Deployment: {}, schema file hash can't be retrieved. Error: schemaSplitTry.length isn't 2, actual length: {}", [ipfsHash, schemaSplitTry.length.toString()])
     }
 
     // We get the first occurrence of `network` since subgraphs can only have data sources for the same network
     let networkSplitTry = manifest.split('network: ', 2)
     if (networkSplitTry.length == 2) {
       let networkSplit = networkSplitTry[1]
-      log.warning("After network: split for {}", [ipfsHash])
       let networkTry = networkSplit.split('\n', 2)
       if (networkTry.length == 2) {
         let network = networkTry[0]
-        log.warning("After \n (network) split for {}", [ipfsHash])
 
         createOrLoadNetwork(network)
         deployment.network = network
@@ -156,8 +148,6 @@ export function fetchSubgraphDeploymentManifest(deployment: SubgraphDeployment, 
     } else {
       log.warning("[MANIFEST PARSING FAIL] Deployment: {}, network can't be parsed. Error: networkSplitTry.length isn't 2, actual length: {}", [ipfsHash, networkSplitTry.length.toString()])
     }
-
-    log.warning("AFTER PARSING LOGIC FOR {}", [ipfsHash])
   }
   {{/ipfs}}
   return deployment as SubgraphDeployment
