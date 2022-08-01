7c7d31fa91a2e05d08c7057bc2d1a1ff5d9b3a6f
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2022-08-01 15:02:51 -0300

    feat: added goerli deployment workflow

---
+++ config/goerliAddressScript.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs'
+import * as mustache from 'mustache'
+import * as networkAddresses from '@graphprotocol/contracts/addresses.json'
+import { Addresses } from './addresses.template'
+
+// mustache doesn't like numbered object keys
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let renameAddresses: any = networkAddresses
+renameAddresses['goerli'] = networkAddresses['5']
+
+export let addresses: Addresses = {
+  controller: '{{goerli.Controller.address}}',
+  graphToken: '{{goerli.GraphToken.address}}',
+  epochManager: '{{goerli.EpochManager.address}}',
+  disputeManager: '{{goerli.DisputeManager.address}}',
+  staking: '{{goerli.Staking.address}}',
+  curation: '{{goerli.Curation.address}}',
+  rewardsManager: '{{goerli.RewardsManager.address}}',
+  serviceRegistry: '{{goerli.ServiceRegistry.address}}',
+  gns: '{{goerli.GNS.address}}',
+  ens: '{{goerli.IENS.address}}',
+  ensPublicResolver: '{{goerli.IPublicResolver.address}}',
+  blockNumber: '',
+  network: '',
+  tokenLockManager: '',
+  subgraphNFT: '{{goerli.SubgraphNFT.address}}',
+}
+
+const main = (): void => {
+  try {
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
+    output.blockNumber = '7210000' // Hardcoded from before first contract deploy of the latest phase
+    output.network = 'goerli'
+    output.tokenLockManager = '0x7B0809048370E69aC0C0844E1188Ecd3aB3A0C5f' // we don't have one, this is rinkebys'
+    output.useTokenLockManager = false
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+  } catch (e) {
+    console.log(`Error saving artifacts: ${e.message}`)
+  }
+}
+
+main()
