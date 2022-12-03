f99a763596ec8db345e4890b3bb5224f6a3789bd
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2022-12-02 21:27:11 -0300

    feat: added mainnet configs and version change

---
+++ config/mainnetArbitrumAddressScript.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs'
+import * as mustache from 'mustache'
+import * as networkAddresses from '@graphprotocol/contracts/addresses.json'
+import { Addresses } from './addresses.template'
+
+// mustache doesn't like numbered object keys
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let renameAddresses: any = networkAddresses
+renameAddresses['arbitrum'] = networkAddresses['42161']
+
+export let addresses: Addresses = {
+  controller: '{{arbitrum.Controller.address}}',
+  graphToken: '{{arbitrum.L2GraphToken.address}}',
+  epochManager: '{{arbitrum.EpochManager.address}}',
+  disputeManager: '{{arbitrum.DisputeManager.address}}',
+  staking: '{{arbitrum.Staking.address}}',
+  curation: '{{arbitrum.Curation.address}}',
+  rewardsManager: '{{arbitrum.RewardsManager.address}}',
+  serviceRegistry: '{{arbitrum.ServiceRegistry.address}}',
+  gns: '{{arbitrum.GNS.address}}',
+  ens: '{{arbitrum.IENS.address}}',
+  ensPublicResolver: '{{arbitrum.IPublicResolver.address}}',
+  blockNumber: '',
+  bridgeBlockNumber: '',
+  network: '',
+  tokenLockManager: '',
+  subgraphNFT: '{{arbitrum.SubgraphNFT.address}}',
+  l1GraphTokenGateway: '',
+  l2GraphTokenGateway: '{{arbitrum.L2GraphTokenGateway.address}}',
+}
+
+const main = (): void => {
+  try {
+    let output = JSON.parse(mustache.render(JSON.stringify(addresses), renameAddresses))
+    output.blockNumber = '42440000' // Hardcoded a few thousand blocks before 1st contract deployed
+    output.network = 'arbitrum-one'
+    output.bridgeBlockNumber = '42449749' // TBD
+    output.tokenLockManager = '0xFCf78AC094288D7200cfdB367A8CD07108dFa128'
+    output.useTokenLockManager = false
+    output.isL1 = false
+    fs.writeFileSync(__dirname + '/generatedAddresses.json', JSON.stringify(output, null, 2))
+  } catch (e) {
+    console.log(`Error saving artifacts: ${e.message}`)
+  }
+}
+
+main()
