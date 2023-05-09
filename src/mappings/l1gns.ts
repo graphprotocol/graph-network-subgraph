65622ddfa3d4873ad009d6f3f85c0b5245db547b
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-05-09 15:28:35 -0300

    feat: added Curator transfer tool support and fixes

--- src/mappings/l1gns.ts
+++ src/mappings/l1gns.ts
@@ -1,11 +1,8 @@
-import { BigInt, BigDecimal, Bytes } from '@graphprotocol/graph-ts'
-import { SubgraphSentToL2 } from '../types/L1GNS/L1GNS'
+import { SubgraphSentToL2, CuratorBalanceSentToL2 } from '../types/L1GNS/L1GNS'
 
-import { Subgraph, SubgraphVersion } from '../types/schema'
+import { Subgraph, NameSignal } from '../types/schema'
 
-import { zeroBD } from './utils'
 import {
-  createOrLoadSubgraph,
   joinID,
   convertBigIntSubgraphIDToBase58,
   getAliasedL2SubgraphID,
@@ -25,11 +22,38 @@ export function handleSubgraphSentToL2(event: SubgraphSentToL2): void {
   let l2id = convertBigIntSubgraphIDToBase58(getAliasedL2SubgraphID(bigIntID))
 
   let subgraph = Subgraph.load(subgraphID)!
-  subgraph.startedMigrationToL2 = true
-  subgraph.startedMigrationToL2At = event.block.timestamp
-  subgraph.startedMigrationToL2AtBlockNumber = event.block.number
-  subgraph.startedMigrationToL2AtTx = event.transaction.hash.toHexString()
+  subgraph.startedTransferToL2 = true
+  subgraph.startedTransferToL2At = event.block.timestamp
+  subgraph.startedTransferToL2AtBlockNumber = event.block.number
+  subgraph.startedTransferToL2AtTx = event.transaction.hash.toHexString()
+  subgraph.signalledTokensSentToL2 = subgraph.signalledTokensSentToL2.plus(event.params._tokens)
   subgraph.idOnL1 = subgraphID
   subgraph.idOnL2 = l2id
+  subgraph.save()
+}
+
+/*
+    event CuratorBalanceSentToL2(
+        uint256 indexed _subgraphID,
+        address indexed _l1Curator,
+        address indexed _l2Beneficiary,
+        uint256 _tokens
+    );
+*/
+export function handleCuratorBalanceSentToL2(event: CuratorBalanceSentToL2): void {
+  let bigIntID = event.params._subgraphID
+  let subgraphID = convertBigIntSubgraphIDToBase58(bigIntID)
+  let subgraphL2id = convertBigIntSubgraphIDToBase58(getAliasedL2SubgraphID(bigIntID))
+  let nameSignalID = joinID([event.params._l1Curator.toHexString(), subgraphID])
+  let nameSignalL2id = joinID([event.params._l2Beneficiary.toHexString(), subgraphL2id])
+
+  let nameSignal = NameSignal.load(nameSignalID)
+  nameSignal.idOnL1 = nameSignalID
+  nameSignal.idOnL2 = nameSignalL2id
+  nameSignal.signalledTokensSentToL2 = nameSignal.signalledTokensSentToL2.plus(event.params._tokens)
+  nameSignal.save()
+
+  let subgraph = Subgraph.load(subgraphID)!
+  subgraph.signalledTokensSentToL2 = subgraph.signalledTokensSentToL2.plus(event.params._tokens)
   subgraph.save()
 }
--- src/mappings/l2gns.ts
+++ src/mappings/l2gns.ts
@@ -1,10 +1,19 @@
-import { BigInt, BigDecimal, Bytes } from '@graphprotocol/graph-ts'
-import { SubgraphReceivedFromL1, SubgraphMigrationFinalized } from '../types/L2GNS/L2GNS'
+import {
+  SubgraphReceivedFromL1,
+  SubgraphMigrationFinalized,
+  CuratorBalanceReturnedToBeneficiary,
+  CuratorBalanceReceived,
+} from '../types/L2GNS/L2GNS'
 
-import { Subgraph, SubgraphVersion } from '../types/schema'
+import { Subgraph, NameSignal } from '../types/schema'
 
-import { zeroBD } from './utils'
-import { createOrLoadSubgraph, joinID, convertBigIntSubgraphIDToBase58 } from './helpers/helpers'
+import {
+  createOrLoadSubgraph,
+  createOrLoadNameSignal,
+  createOrLoadGraphAccount,
+  joinID,
+  convertBigIntSubgraphIDToBase58,
+} from './helpers/helpers'
 
 /*
     event SubgraphReceivedFromL1(
@@ -24,10 +33,10 @@ export function handleSubgraphReceivedFromL1(event: SubgraphReceivedFromL1): voi
     event.params._owner,
     event.block.timestamp,
   )
-  subgraph.startedMigrationToL2 = true
-  subgraph.startedMigrationToL2At = event.block.timestamp
-  subgraph.startedMigrationToL2AtBlockNumber = event.block.number
-  subgraph.startedMigrationToL2AtTx = event.transaction.hash.toHexString()
+  subgraph.startedTransferToL2 = true
+  subgraph.startedTransferToL2At = event.block.timestamp
+  subgraph.startedTransferToL2AtBlockNumber = event.block.number
+  subgraph.startedTransferToL2AtTx = event.transaction.hash.toHexString()
   subgraph.idOnL1 = convertBigIntSubgraphIDToBase58(event.params._l1SubgraphID)
   subgraph.idOnL2 = convertBigIntSubgraphIDToBase58(event.params._l2SubgraphID)
   subgraph.save()
@@ -39,9 +48,50 @@ export function handleSubgraphMigrationFinalized(event: SubgraphMigrationFinaliz
   let subgraphID = convertBigIntSubgraphIDToBase58(bigIntID)
   // Can't use createOrLoadSubgraph, loading it directly, as it's also assumed already created
   let subgraph = Subgraph.load(subgraphID)!
-  subgraph.migratedToL2 = true
-  subgraph.migratedToL2At = event.block.timestamp
-  subgraph.migratedToL2AtBlockNumber = event.block.number
-  subgraph.migratedToL2AtTx = event.transaction.hash.toHexString()
+  subgraph.transferredToL2 = true
+  subgraph.transferredToL2At = event.block.timestamp
+  subgraph.transferredToL2AtBlockNumber = event.block.number
+  subgraph.transferredToL2AtTx = event.transaction.hash.toHexString()
   subgraph.save()
+}
+
+/// @dev Emitted when the L1 balance for a curator has been claimed
+// event CuratorBalanceReceived(uint256 _l2SubgraphID, address _l2Curator, uint256 _tokens);
+
+export function handleCuratorBalanceReceived(event: CuratorBalanceReceived): void {
+  let bigIntID = event.params._l2SubgraphID
+  let subgraphID = convertBigIntSubgraphIDToBase58(bigIntID)
+
+  let nameSignal = createOrLoadNameSignal(
+    event.params._l2Curator.toHexString(),
+    subgraphID,
+    event.block.timestamp,
+  )
+  nameSignal.idOnL2 = nameSignal.id
+  nameSignal.signalledTokensReceivedOnL2 = nameSignal.signalledTokensReceivedOnL2.plus(
+    event.params._tokens,
+  )
+  nameSignal.save()
+
+  let subgraph = Subgraph.load(subgraphID)!
+  subgraph.signalledTokensReceivedOnL2 = subgraph.signalledTokensReceivedOnL2.plus(
+    event.params._tokens,
+  )
+  subgraph.save()
+}
+
+/// @dev Emitted when the L1 balance for a curator has been returned to the beneficiary.
+/// This can happen if the subgraph migration was not finished when the curator's tokens arrived.
+// event CuratorBalanceReturnedToBeneficiary(
+//     uint256 _subgraphID,
+//     address _l2Curator,
+//     uint256 _tokens
+// );
+
+export function handleCuratorBalanceReturnedToBeneficiary(
+  event: CuratorBalanceReturnedToBeneficiary,
+): void {
+  let graphAccount = createOrLoadGraphAccount(event.params._l2Curator, event.block.timestamp)
+  graphAccount.balanceReceivedFromL1 = graphAccount.balanceReceivedFromL1.plus(event.params._tokens)
+  graphAccount.save()
 }
