d28479020b2864f92d0cc288c0f40529b87072bf
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-06-07 19:32:20 -0300

    feat: backwards compatibility, SubgraphMetadata and other fixes

--- src/mappings/ipfs.ts
+++ src/mappings/ipfs.ts
@@ -5,21 +5,28 @@ import {
   GraphAccountMetadata,
   SubgraphDeploymentSchema,
   SubgraphDeploymentManifest,
-  Subgraph
+  Subgraph,
+  GraphAccount
 } from '../types/schema'
 import { jsonToString } from './utils'
 import { createOrLoadSubgraphCategory, createOrLoadSubgraphCategoryRelation } from './helpers/helpers'
 
 export function handleSubgraphMetadata(content: Bytes): void {
   let subgraphMetadata = SubgraphMetadata.load(dataSource.stringParam())
-  let subgraph = Subgraph.load(SubgraphMetadata.subgraph)
+  let subgraph = Subgraph.load(subgraphMetadata.subgraph)
   let tryData = json.try_fromBytes(content)
   if (tryData.isOk) {
     let data = tryData.value.toObject()
+    // Duplicating the fields for backwards compatibility
     subgraphMetadata.description = jsonToString(data.get('description'))
     subgraphMetadata.displayName = jsonToString(data.get('displayName'))
     subgraphMetadata.codeRepository = jsonToString(data.get('codeRepository'))
     subgraphMetadata.website = jsonToString(data.get('website'))
+
+    subgraph.description = subgraphMetadata.description
+    subgraph.displayName = subgraphMetadata.displayName
+    subgraph.codeRepository = subgraphMetadata.codeRepository
+    subgraph.website = subgraphMetadata.website
     let categories = data.get('categories')
 
     if (categories != null && !categories.isNull()) {
@@ -39,9 +46,14 @@ export function handleSubgraphMetadata(content: Bytes): void {
     if (subgraphImage != null && subgraphImage.kind === JSONValueKind.STRING) {
       subgraphMetadata.nftImage = image
       subgraphMetadata.image = jsonToString(subgraphImage)
+      subgraph.nftImage = subgraphMetadata.nftImage
+      subgraph.image = subgraphMetadata.image
     } else {
       subgraphMetadata.image = image
+      subgraph.image = subgraphMetadata.image
     }
+    subgraphMetadata.save()
+    subgraph.save()
   }
 }
 
@@ -55,6 +67,7 @@ export function handleSubgraphVersionMetadata(content: Bytes): void {
 
 export function handleGraphAccountMetadata(content: Bytes): void {
   let graphAccountMetadata = GraphAccountMetadata.load(dataSource.stringParam())
+  let graphAccount = GraphAccount.load(graphAccountMetadata.graphAccount)!
   let tryData = json.try_fromBytes(content)
   if (tryData.isOk) {
     let data = tryData.value.toObject()
@@ -62,12 +75,20 @@ export function handleGraphAccountMetadata(content: Bytes): void {
     graphAccountMetadata.description = jsonToString(data.get('description'))
     graphAccountMetadata.image = jsonToString(data.get('image'))
     graphAccountMetadata.displayName = jsonToString(data.get('displayName'))
+    graphAccount.codeRepository = graphAccountMetadata.codeRepository
+    graphAccount.description = graphAccountMetadata.description
+    graphAccount.image = graphAccountMetadata.image
+    graphAccount.displayName = graphAccountMetadata.displayName
     let isOrganization = data.get('isOrganization')
     if (isOrganization != null && isOrganization.kind === JSONValueKind.BOOL) {
       graphAccountMetadata.isOrganization = isOrganization.toBool()
+      graphAccount.isOrganization = graphAccountMetadata.isOrganization
     }
     graphAccountMetadata.website = jsonToString(data.get('website'))
+    graphAccount.website = graphAccountMetadata.website
+
     graphAccountMetadata.save()
+    graphAccount.save()
   }
 }
 
