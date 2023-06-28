61fb069ce9ea9e642a6ea982a0eb2bfc43c7d083
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2023-06-28 18:07:12 -0300

    fix: leftover state inconsistencies

--- src/mappings/l1gns.ts
+++ src/mappings/l1gns.ts
@@ -1,3 +1,4 @@
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { SubgraphSentToL2, CuratorBalanceSentToL2 } from '../types/L1GNS/L1GNS'
 
 import { Subgraph, NameSignal, SubgraphVersion, SubgraphDeployment } from '../types/schema'
@@ -20,8 +21,24 @@ export function handleSubgraphSentToL2(event: SubgraphSentToL2): void {
   let bigIntID = event.params._subgraphID
   let subgraphID = convertBigIntSubgraphIDToBase58(bigIntID)
   let l2id = convertBigIntSubgraphIDToBase58(getAliasedL2SubgraphID(bigIntID))
+  let nameSignalID = joinID([event.params._l1Owner.toHexString(), subgraphID])
+  let nameSignalL2id = joinID([event.params._l2Owner.toHexString(), l2id])
+
+  let nameSignal = NameSignal.load(nameSignalID)!
+  nameSignal.transferredToL2 = true
+  nameSignal.transferredToL2At = event.block.timestamp
+  nameSignal.transferredToL2AtBlockNumber = event.block.number
+  nameSignal.transferredToL2AtTx = event.transaction.hash.toHexString()
+  nameSignal.idOnL1 = nameSignalID
+  nameSignal.idOnL2 = nameSignalL2id
+  nameSignal.signalledTokensSentToL2 = nameSignal.signalledTokensSentToL2.plus(event.params._tokens)
+
+  // uint256 tokensForL2 = ownerNSignal.mul(curationTokens).div(totalSignal);
+  // tokensForL2.mul(totalSignal) = ownerNSignal.mul(curationTokens)
+  // tokensForL2.mul(totalSignal).div(ownerNSignal) = curationTokens
 
   let subgraph = Subgraph.load(subgraphID)!
+  subgraph.active = false;
   subgraph.startedTransferToL2 = true
   subgraph.startedTransferToL2At = event.block.timestamp
   subgraph.startedTransferToL2AtBlockNumber = event.block.number
@@ -29,6 +46,17 @@ export function handleSubgraphSentToL2(event: SubgraphSentToL2): void {
   subgraph.signalledTokensSentToL2 = subgraph.signalledTokensSentToL2.plus(event.params._tokens)
   subgraph.idOnL1 = subgraphID
   subgraph.idOnL2 = l2id
+  
+  let curationTokens = event.params._tokens.times(subgraph.nameSignalAmount).div(nameSignal.nameSignal)
+
+  subgraph.nameSignalAmount = subgraph.nameSignalAmount.minus(nameSignal.nameSignal)
+  subgraph.withdrawableTokens = curationTokens.minus(event.params._tokens)
+
+  nameSignal.nameSignal = BigInt.fromI32(0);
+  nameSignal.signal = BigDecimal.fromString('0');
+  nameSignal.lastNameSignalChange = event.block.number.toI32()
+
+  nameSignal.save()
   subgraph.save()
 
   let version = SubgraphVersion.load(subgraph.currentVersion!)!
@@ -64,10 +92,18 @@ export function handleCuratorBalanceSentToL2(event: CuratorBalanceSentToL2): voi
   nameSignal.idOnL1 = nameSignalID
   nameSignal.idOnL2 = nameSignalL2id
   nameSignal.signalledTokensSentToL2 = nameSignal.signalledTokensSentToL2.plus(event.params._tokens)
-  nameSignal.save()
-
+  
   let subgraph = Subgraph.load(subgraphID)!
   subgraph.signalledTokensSentToL2 = subgraph.signalledTokensSentToL2.plus(event.params._tokens)
+
+  subgraph.nameSignalAmount = subgraph.nameSignalAmount.minus(nameSignal.nameSignal)
+  subgraph.withdrawableTokens = subgraph.withdrawableTokens.minus(event.params._tokens)
+
+  nameSignal.nameSignal = BigInt.fromI32(0);
+  nameSignal.signal = BigDecimal.fromString('0');
+  nameSignal.lastNameSignalChange = event.block.number.toI32()
+
+  nameSignal.save()
   subgraph.save()
 
   let version = SubgraphVersion.load(subgraph.currentVersion!)!
