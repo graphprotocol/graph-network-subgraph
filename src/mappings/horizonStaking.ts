8089aedd4382bd2e90bc527052f632c5d102a183
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2025-02-24 01:42:00 -0300

    Revert "fix: more fixes from reviews, and fixed addresses import preventing build"
    This reverts commit 54b4ddf97eee82e5ea313f107285d846cdb9cdee.

--- src/mappings/horizonStaking.ts
+++ src/mappings/horizonStaking.ts
@@ -1,11 +1,12 @@
 import { BigInt } from '@graphprotocol/graph-ts'
-import { addresses } from '../../config/addresses'
-import { HorizonStakeDeposited, HorizonStakeLocked, HorizonStakeWithdrawn } from '../types/HorizonStaking/HorizonStaking'
+import { addresses } from '../../config/addresses.template'
+import { HorizonStakeDeposited, HorizonStakeLocked, HorizonStakeWithdrawn} from '../types/HorizonStaking/HorizonStaking'
 import { Indexer } from '../types/schema'
 import { calculateCapacities, createOrLoadEpoch, createOrLoadGraphNetwork, createOrLoadIndexer, updateAdvancedIndexerMetrics } from './helpers/helpers'
 
 
 export function handleHorizonStakeDeposited(event: HorizonStakeDeposited): void {
+    // To Do
     let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
     // update indexer
     let indexer = createOrLoadIndexer(event.params.serviceProvider, event.block.timestamp)
@@ -14,61 +15,64 @@ export function handleHorizonStakeDeposited(event: HorizonStakeDeposited): void 
     indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
     indexer = calculateCapacities(indexer as Indexer)
     indexer.save()
-
+  
     // Update graph network
     graphNetwork.totalTokensStaked = graphNetwork.totalTokensStaked.plus(event.params.tokens)
-    if (previousStake.isZero()) {
-        graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount + 1
+    if (previousStake == BigInt.fromI32(0)) {
+      graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount + 1
     }
     graphNetwork.save()
-
+  
     // Update epoch
     let epoch = createOrLoadEpoch(
-        addresses.isL1 ? event.block.number : graphNetwork.currentL1BlockNumber!,
+      addresses.isL1 ? event.block.number : graphNetwork.currentL1BlockNumber!,
     )
     epoch.stakeDeposited = epoch.stakeDeposited.plus(event.params.tokens)
     epoch.save()
 }
 
 export function handleHorizonStakeLocked(event: HorizonStakeLocked): void {
-    let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
-    // update indexer
-    let id = event.params.serviceProvider.toHexString()
-    let indexer = Indexer.load(id)!
-    indexer.lockedTokens = event.params.tokens
-    indexer.tokensLockedUntil = event.params.until.toI32()
-    indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
-    indexer = calculateCapacities(indexer as Indexer)
-    indexer.save()
+  // To Do
+  let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
+  // update indexer
+  let id = event.params.serviceProvider.toHexString()
+  let indexer = Indexer.load(id)!
+  let oldLockedTokens = indexer.lockedTokens
+  indexer.lockedTokens = event.params.tokens
+  indexer.tokensLockedUntil = event.params.until.toI32()
+  indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
+  indexer = calculateCapacities(indexer as Indexer)
+  indexer.save()
 
-    // update graph network
-    graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.plus(
-        event.params.tokens,
-    )
-    graphNetwork.save()
+  // update graph network
+  // the tokens from the event replace the previously locked tokens
+  // from this indexer
+  graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.plus(
+    event.params.tokens,
+  ).minus(oldLockedTokens)
+  if (indexer.stakedTokens == indexer.lockedTokens) {
+    graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount - 1
+  }
+  graphNetwork.save()
 }
 
 export function handleHorizonStakeWithdrawn(event: HorizonStakeWithdrawn): void {
-    let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
-    // update indexer
-    let id = event.params.serviceProvider.toHexString()
-    let indexer = Indexer.load(id)!
-    indexer.stakedTokens = indexer.stakedTokens.minus(event.params.tokens)
-    indexer.lockedTokens = BigInt.fromI32(0) // set to 0 to prevent issues when Stage 2 comes
-    indexer.tokensLockedUntil = 0 // always set to 0 when withdrawn
-    indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
-    indexer = calculateCapacities(indexer as Indexer)
-    indexer.save()
+  // To Do
+  let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
+  // update indexer
+  let id = event.params.serviceProvider.toHexString()
+  let indexer = Indexer.load(id)!
+  indexer.stakedTokens = indexer.stakedTokens.minus(event.params.tokens)
+  indexer.lockedTokens = BigInt.fromI32(0) // set to 0 to prevent issues when Stage 2 comes
+  indexer.tokensLockedUntil = 0 // always set to 0 when withdrawn
+  indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
+  indexer = calculateCapacities(indexer as Indexer)
+  indexer.save()
 
-    // Update graph network
-    graphNetwork.totalTokensStaked = graphNetwork.totalTokensStaked.minus(event.params.tokens)
-    graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.minus(
-        event.params.tokens,
-    )
-    // We might want to introduce the notion of "provisioned indexer count" or "active indexer count" 
-    // to each data service since that's what this particular count wanted to convey.
-    if (indexer.stakedTokens.isZero()) {
-        graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount - 1
-    }
-    graphNetwork.save()
+  // Update graph network
+  graphNetwork.totalTokensStaked = graphNetwork.totalTokensStaked.minus(event.params.tokens)
+  graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.minus(
+    event.params.tokens,
+  )
+  graphNetwork.save()
 }
--- src/mappings/staking.ts
+++ src/mappings/staking.ts
@@ -106,6 +106,7 @@ export function handleStakeLocked(event: StakeLocked): void {
   // update indexer
   let id = event.params.indexer.toHexString()
   let indexer = Indexer.load(id)!
+  let oldLockedTokens = indexer.lockedTokens
   indexer.lockedTokens = event.params.tokens
   indexer.tokensLockedUntil = event.params.until.toI32()
   indexer = updateAdvancedIndexerMetrics(indexer as Indexer)
@@ -113,9 +114,11 @@ export function handleStakeLocked(event: StakeLocked): void {
   indexer.save()
 
   // update graph network
+  // the tokens from the event replace the previously locked tokens
+  // from this indexer
   graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.plus(
     event.params.tokens,
-  )
+  ).minus(oldLockedTokens)
   if (indexer.stakedTokens == indexer.lockedTokens) {
     graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount - 1
   }
