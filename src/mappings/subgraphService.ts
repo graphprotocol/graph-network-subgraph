c618aec3fccfa25be91ae685d56e22ac56f829ac
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2025-02-24 04:05:33 -0300

    feat: implemented AllocationCreated, Closed and Resized

--- src/mappings/subgraphService.ts
+++ src/mappings/subgraphService.ts
@@ -1,6 +1,8 @@
-import { ethereum, ValueKind, log } from "@graphprotocol/graph-ts"
+import { BigDecimal, BigInt, ethereum, log } from "@graphprotocol/graph-ts"
 import { AllocationClosed, AllocationCreated, AllocationResized, RewardsDestinationSet, ServiceProviderRegistered } from "../types/SubgraphService/SubgraphService"
-import { createOrLoadProvision } from "./helpers/helpers"
+import { createOrLoadEpoch, createOrLoadGraphNetwork, createOrLoadProvision, createOrLoadSubgraphDeployment } from "./helpers/helpers"
+import { Allocation, GraphAccount, Indexer } from "../types/schema"
+import { addresses } from "../../config/addresses"
 
 export function handleServiceProviderRegistered(event: ServiceProviderRegistered): void {
     let decodedCalldata = ethereum.decode('(string,string,address)', event.params.data)
@@ -21,22 +23,147 @@ export function handleRewardsDestinationSet(event: RewardsDestinationSet): void 
     provision.save()
 }
 
-export function handleAllocationClosed(event: AllocationClosed): void {
-    let provision = createOrLoadProvision(event.params.indexer, event.address, event.block.timestamp)
-    // To Do
-    provision.save()
-}
-
-
 export function handleAllocationCreated(event: AllocationCreated): void {
+    let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
+    let subgraphDeploymentID = event.params.subgraphDeploymentId.toHexString()
+    let indexerID = event.params.indexer.toHexString()
+    let allocationID = event.params.allocationId.toHexString()
+
+    // update indexer
+    let indexer = Indexer.load(indexerID)!
+    indexer.allocatedTokens = indexer.allocatedTokens.plus(event.params.tokens)
+    indexer.totalAllocationCount = indexer.totalAllocationCount.plus(BigInt.fromI32(1))
+    indexer.allocationCount = indexer.allocationCount + 1
+    indexer.save()
+
+    // update provision
     let provision = createOrLoadProvision(event.params.indexer, event.address, event.block.timestamp)
-    // To Do
+    provision.tokensAllocated = provision.tokensAllocated.plus(event.params.tokens)
+    provision.totalAllocationCount = provision.totalAllocationCount.plus(BigInt.fromI32(1))
+    provision.allocationCount = provision.allocationCount + 1
     provision.save()
+
+    // update graph network
+    graphNetwork.totalTokensAllocated = graphNetwork.totalTokensAllocated.plus(event.params.tokens)
+    graphNetwork.allocationCount = graphNetwork.allocationCount + 1
+    graphNetwork.activeAllocationCount = graphNetwork.activeAllocationCount + 1
+    graphNetwork.save()
+
+    // update subgraph deployment
+    let deployment = createOrLoadSubgraphDeployment(subgraphDeploymentID, event.block.timestamp)
+    deployment.stakedTokens = deployment.stakedTokens.plus(event.params.tokens)
+    deployment.save()
+
+    // create allocation
+    let allocation = new Allocation(allocationID)
+    allocation.indexer = indexerID
+    allocation.provision = provision.id
+    allocation.creator = event.transaction.from
+    allocation.activeForIndexer = indexerID
+    allocation.activeForProvision = provision.id
+    allocation.subgraphDeployment = subgraphDeploymentID
+    allocation.allocatedTokens = event.params.tokens
+    allocation.effectiveAllocation = BigInt.fromI32(0)
+    allocation.createdAtEpoch = event.params.currentEpoch.toI32()
+    allocation.createdAtBlockHash = event.block.hash
+    allocation.createdAtBlockNumber = (
+        addresses.isL1 ? event.block.number : graphNetwork.currentL1BlockNumber!
+    ).toI32()
+    allocation.queryFeesCollected = BigInt.fromI32(0)
+    allocation.queryFeeRebates = BigInt.fromI32(0)
+    allocation.distributedRebates = BigInt.fromI32(0)
+    allocation.curatorRewards = BigInt.fromI32(0)
+    allocation.indexingRewards = BigInt.fromI32(0)
+    allocation.indexingIndexerRewards = BigInt.fromI32(0)
+    allocation.indexingDelegatorRewards = BigInt.fromI32(0)
+    allocation.delegationFees = BigInt.fromI32(0)
+    allocation.status = 'Active'
+    allocation.totalReturn = BigDecimal.fromString('0')
+    allocation.annualizedReturn = BigDecimal.fromString('0')
+    allocation.createdAt = event.block.timestamp.toI32()
+    allocation.indexingRewardCutAtStart = provision.indexingRewardsCut
+    allocation.indexingRewardEffectiveCutAtStart = provision.indexingRewardsCut.toBigDecimal()
+    allocation.queryFeeCutAtStart = provision.queryFeeCut
+    allocation.queryFeeEffectiveCutAtStart = provision.queryFeeCut.toBigDecimal()
+    allocation.save()
 }
 
+export function handleAllocationClosed(event: AllocationClosed): void {
+    let graphNetwork = createOrLoadGraphNetwork(event.block.number, event.address)
+    let indexerID = event.params.indexer.toHexString()
+    let allocationID = event.params.allocationId.toHexString()
+
+    // update indexer
+    let indexer = Indexer.load(indexerID)!
+    const indexerAccount = GraphAccount.load(indexer.account)!
+    const closedByIndexer = event.transaction.from == event.params.indexer
+    const closedByOperator = indexerAccount.operators.includes(event.transaction.from.toHexString())
+
+    if (!closedByIndexer && !closedByOperator) {
+        indexer.forcedClosures = indexer.forcedClosures + 1
+    }
+    indexer.allocatedTokens = indexer.allocatedTokens.minus(event.params.tokens)
+    indexer.allocationCount = indexer.allocationCount - 1
+    indexer.save()
+
+    // update provision
+    let provision = createOrLoadProvision(event.params.indexer, event.address, event.block.timestamp)
+    provision.tokensAllocated = provision.tokensAllocated.minus(event.params.tokens)
+    provision.allocationCount = provision.allocationCount - 1
+    provision.save()
+
+    // update allocation
+    let allocation = Allocation.load(allocationID)!
+    allocation.poolClosedIn = graphNetwork.currentEpoch
+    allocation.activeForIndexer = null
+    allocation.closedAtEpoch = graphNetwork.currentEpoch
+    allocation.closedAtBlockHash = event.block.hash
+    allocation.closedAtBlockNumber = (
+        addresses.isL1 ? event.block.number : graphNetwork.currentL1BlockNumber!
+    ).toI32()
+    allocation.status = 'Closed'
+    allocation.closedAt = event.block.timestamp.toI32()
+    allocation.indexingRewardCutAtStart = provision.indexingRewardsCut
+    allocation.indexingRewardEffectiveCutAtStart = provision.indexingRewardsCut.toBigDecimal()
+    allocation.queryFeeCutAtStart = provision.queryFeeCut
+    allocation.queryFeeEffectiveCutAtStart = provision.queryFeeCut.toBigDecimal()
+    allocation.save()
+
+    // update epoch - We do it here to have more epochs created, instead of seeing none created
+    // Likely this problem would go away with a live network with long epochs
+    let epoch = createOrLoadEpoch(
+        addresses.isL1 ? event.block.number : graphNetwork.currentL1BlockNumber!,
+    )
+    epoch.save()
+
+    let subgraphDeploymentID = event.params.subgraphDeploymentId.toHexString()
+    let deployment = createOrLoadSubgraphDeployment(subgraphDeploymentID, event.block.timestamp)
+    deployment.stakedTokens = deployment.stakedTokens.minus(event.params.tokens)
+    deployment.save()
+
+    // update graph network
+    graphNetwork.activeAllocationCount = graphNetwork.activeAllocationCount - 1
+    graphNetwork.totalTokensAllocated = graphNetwork.totalTokensAllocated.minus(event.params.tokens)
+    graphNetwork.save()
+}
 
 export function handleAllocationResized(event: AllocationResized): void {
+    let allocationID = event.params.allocationId.toHexString()
+    let indexerID = event.params.indexer.toHexString()
+    let diffTokens = event.params.newTokens.minus(event.params.oldTokens)
+
+    // update indexer
+    let indexer = Indexer.load(indexerID)!
+    indexer.allocatedTokens = indexer.allocatedTokens.plus(diffTokens)
+    indexer.save()
+
+    // update provision
     let provision = createOrLoadProvision(event.params.indexer, event.address, event.block.timestamp)
-    // To Do
+    provision.tokensAllocated = provision.tokensAllocated.plus(diffTokens)
     provision.save()
+
+    // update allocation
+    let allocation = Allocation.load(allocationID)!
+    allocation.allocatedTokens = event.params.newTokens
+    allocation.save()
 }
