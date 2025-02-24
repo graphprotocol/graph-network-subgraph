3dded978204b951b36712f80749cc11804ad98a8
Juan Manuel Rodriguez Defago <juanma.rdefago@gmail.com>
2025-02-24 04:12:42 -0300

    fix: abi.decode potential decoding failure

--- src/mappings/subgraphService.ts
+++ src/mappings/subgraphService.ts
@@ -1,13 +1,18 @@
-import { ethereum } from "@graphprotocol/graph-ts"
+import { ethereum, ValueKind, log } from "@graphprotocol/graph-ts"
 import { RewardsDestinationSet, ServiceProviderRegistered } from "../types/SubgraphService/SubgraphService"
 import { createOrLoadProvision } from "./helpers/helpers"
 
 export function handleServiceProviderRegistered(event: ServiceProviderRegistered): void {
-    let decodedCalldata = ethereum.decode('(string,string,address)', event.params.data).toTuple()
-    let provision = createOrLoadProvision(event.params.serviceProvider, event.address, event.block.timestamp)
-    provision.url = decodedCalldata[0].toString()
-    provision.geoHash = decodedCalldata[1].toString()
-    provision.save()
+    let decodedCalldata = ethereum.decode('(string,string,address)', event.params.data)
+    if( decodedCalldata != null && decodedCalldata.kind == ethereum.ValueKind.TUPLE) {
+        let tupleData = decodedCalldata.toTuple()
+        let provision = createOrLoadProvision(event.params.serviceProvider, event.address, event.block.timestamp)
+        provision.url = tupleData[0].toString()
+        provision.geoHash = tupleData[1].toString()
+        provision.save()
+    } else {
+        log.warning("ServiceProviderRegistered failed to decode: {}", [event.params.data.toHexString()])
+    }
 }
 
 export function handleRewardsDestinationSet(event: RewardsDestinationSet): void {
