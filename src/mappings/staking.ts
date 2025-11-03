3c959a608ca4a549823933b6016b6148360156d6
Tomás Migone <tomas@edgeandnode.com>
2025-11-03 16:57:20 -0300

    fix: proper tracking of legacy locked tokens vs locked tokens
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- src/mappings/staking.ts
+++ src/mappings/staking.ts
@@ -98,6 +98,7 @@ export function handleStakeDeposited(event: StakeDeposited): void {
 
 /**
  * @dev handleStakeLocked
+ * Handler for legacy stake locking
  * - updated the Indexers stake
  * - note - the contracts work by not changing the tokensStaked amount, so here, capacity does not
  *          get changed
@@ -107,8 +108,6 @@ export function handleStakeLocked(event: StakeLocked): void {
   // update indexer
   let id = event.params.indexer.toHexString()
   let indexer = Indexer.load(id)!
-  indexer.lockedTokens = event.params.tokens
-  indexer.tokensLockedUntil = event.params.until.toI32()
   indexer.legacyLockedTokens = event.params.tokens
   indexer.legacyTokensLockedUntil = event.params.until.toI32()
   indexer = updateLegacyAdvancedIndexerMetrics(indexer as Indexer)
@@ -119,7 +118,7 @@ export function handleStakeLocked(event: StakeLocked): void {
   graphNetwork.totalUnstakedTokensLocked = graphNetwork.totalUnstakedTokensLocked.plus(
     event.params.tokens,
   )
-  if (indexer.stakedTokens == indexer.lockedTokens) {
+  if (indexer.stakedTokens == indexer.lockedTokens.plus(indexer.legacyLockedTokens)) {
     graphNetwork.stakedIndexersCount = graphNetwork.stakedIndexersCount - 1
   }
   graphNetwork.save()
@@ -127,6 +126,7 @@ export function handleStakeLocked(event: StakeLocked): void {
 
 /**
  * @dev handleStakeWithdrawn
+ * Handler for legacy stake withdrawal
  * - updated the Indexers stake
  * - updates the GraphNetwork total stake
  */
@@ -136,8 +136,6 @@ export function handleStakeWithdrawn(event: StakeWithdrawn): void {
   let id = event.params.indexer.toHexString()
   let indexer = Indexer.load(id)!
   indexer.stakedTokens = indexer.stakedTokens.minus(event.params.tokens)
-  indexer.lockedTokens = indexer.lockedTokens.minus(event.params.tokens)
-  indexer.tokensLockedUntil = 0 // always set to 0 when withdrawn
   indexer.legacyLockedTokens = indexer.legacyLockedTokens.minus(event.params.tokens)
   indexer.legacyTokensLockedUntil = 0 // always set to 0 when withdrawn
   indexer = updateLegacyAdvancedIndexerMetrics(indexer as Indexer)
@@ -178,7 +176,6 @@ export function handleStakeSlashed(event: StakeSlashed): void {
         : indexer.legacyLockedTokens
 
       indexer.legacyLockedTokens = indexer.legacyLockedTokens.minus(tokensToUnlock)
-      indexer.lockedTokens = indexer.lockedTokens.minus(tokensToUnlock)
 
       if (indexer.legacyLockedTokens.equals(BigInt.fromI32(0))) {
         indexer.legacyTokensLockedUntil = 0
@@ -292,9 +289,7 @@ export function handleStakeDelegatedLocked(event: StakeDelegatedLocked): void {
 
   delegatedStake.unstakedTokens = delegatedStake.unstakedTokens.plus(event.params.tokens)
   delegatedStake.shareAmount = delegatedStake.shareAmount.minus(event.params.shares)
-  delegatedStake.lockedTokens = delegatedStake.lockedTokens.plus(event.params.tokens)
   delegatedStake.legacyLockedTokens = delegatedStake.legacyLockedTokens.plus(event.params.tokens)
-  delegatedStake.lockedUntil = event.params.until.toI32() // until always updates and overwrites the past lockedUntil time
   delegatedStake.legacyLockedUntil = event.params.until.toI32() // until always updates and overwrites the past lockedUntil time
   delegatedStake.lastUndelegatedAt = event.block.timestamp.toI32()
 
@@ -332,9 +327,7 @@ export function handleStakeDelegatedWithdrawn(event: StakeDelegatedWithdrawn): v
   let delegatorID = event.params.delegator.toHexString()
   let id = joinID([delegatorID, indexerID])
   let delegatedStake = DelegatedStake.load(id)!
-  delegatedStake.lockedTokens = BigInt.fromI32(0)
   delegatedStake.legacyLockedTokens = BigInt.fromI32(0)
-  delegatedStake.lockedUntil = 0
   delegatedStake.legacyLockedUntil = 0
   delegatedStake.save()
 }
